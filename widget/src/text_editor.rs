b9821399280576d2decfa3fb5f21d4427d8123ae
Andrew Baldwin <andrew@boldfriend.org>
2025-11-25 13:52:18 -0800

    Create a selection in text editor when shift-clicking away from the current cursor position.

--- widget/src/text_editor.rs
+++ widget/src/text_editor.rs
@@ -706,14 +706,20 @@ where
             self.key_binding.as_deref(),
         ) {
             match update {
-                Update::Click(click) => {
+                Update::Click(click, modifiers) => {
                     state.focus = Some(Focus::now());
                     state.last_click = Some(click);
 
                     match click.button() {
                         mouse::Button::Left => {
                             let action = match click.kind() {
-                                mouse::click::Kind::Single => Action::Click(click.position()),
+                                mouse::click::Kind::Single => {
+                                    if modifiers.shift() {
+                                        Action::Drag(click.position())
+                                    } else {
+                                        Action::Click(click.position())
+                                    }
+                                }
                                 mouse::click::Kind::Double => Action::SelectWord,
                                 mouse::click::Kind::Triple => Action::SelectLine,
                             };
@@ -735,9 +741,16 @@ where
                 Update::Drag(position) => {
                     shell.publish(on_edit(Action::Drag(position)));
                 }
-                Update::Release => {
+                Update::ButtonRelease => {
                     state.drag_click = None;
 
+                    if cfg!(target_os = "linux") && state.focus.is_some() {
+                        shell.write_clipboard_primary(clipboard::Content::Text(
+                            self.content.selection().unwrap_or_default(),
+                        ));
+                    }
+                }
+                Update::KeyRelease => {
                     if cfg!(target_os = "linux") && state.focus.is_some() {
                         shell.write_clipboard_primary(clipboard::Content::Text(
                             self.content.selection().unwrap_or_default(),
@@ -1201,9 +1214,10 @@ impl<Message> Binding<Message> {
 }
 
 enum Update<Message> {
-    Click(mouse::Click),
+    Click(mouse::Click, keyboard::Modifiers),
     Drag(Point),
-    Release,
+    ButtonRelease,
+    KeyRelease,
     Scroll(f32),
     InputMethod(Ime),
     Binding(Binding<Message>),
@@ -1231,7 +1245,7 @@ impl<Message> Update<Message> {
 
         match event {
             Event::Mouse(event) => match event {
-                mouse::Event::ButtonPressed { button, .. }
+                mouse::Event::ButtonPressed { button, modifiers }
                     if matches!(button, mouse::Button::Left)
                         || (cfg!(target_os = "linux")
                             && matches!(button, mouse::Button::Middle)) =>
@@ -1242,14 +1256,14 @@ impl<Message> Update<Message> {
 
                         let click = mouse::Click::new(cursor_position, *button, state.last_click);
 
-                        Some(Update::Click(click))
+                        Some(Update::Click(click, *modifiers))
                     } else if state.focus.is_some() {
                         binding(Binding::Unfocus)
                     } else {
                         None
                     }
                 }
-                mouse::Event::ButtonReleased(mouse::Button::Left) => Some(Update::Release),
+                mouse::Event::ButtonReleased(mouse::Button::Left) => Some(Update::ButtonRelease),
                 mouse::Event::CursorMoved { .. } => match state.drag_click {
                     Some(mouse::click::Kind::Single) => {
                         let cursor_position =
@@ -1321,7 +1335,7 @@ impl<Message> Update<Message> {
                 .map(Self::Binding)
             }
             Event::Keyboard(keyboard::Event::KeyReleased { .. }) if cfg!(target_os = "linux") => {
-                Some(Update::Release)
+                Some(Update::KeyRelease)
             }
             _ => None,
         }
